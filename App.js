502aaafe1819454ca2163e02954536ecd59947dc
rafasarzi <rajustino@gmail.com>
2022-09-14 10:31:12 -0300

    feat:( add navegation )

--- App.js
+++ App.js
@@ -1,50 +1,60 @@
-import React from 'react';
-import {  TouchableOpacity, Text, StyleSheet } from 'react-native';
-import Topo from './src/telas/viva/components/topo';
+import * as React from 'react';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Topo from './src/telas/viva/components/topo'
 
-export default function App() {
-  
+function HomeScreen({ navigation }) {
   return (
-    <>
-
-    
-    <Topo/>
-
-    <TouchableOpacity style={styles.button} onPress={() => alert('Pressed')}>
-      <Text>Touchable Opacity</Text>
-    </TouchableOpacity>
-
-    <TouchableOpacity
-        style={styles.button}
-        onPress={() => alert('Pressed')}
-      >
-       <Text>Cadastro Cliente</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Topo/>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Details')}>
+        <Text>Cadastro Cliente</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => alert('Pressed')}
-      >
+    </View>
+  );
+}
+
+function DetailsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Topo/>
+      <Text>Details Screen</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
         <Text>Cadatro Instrutor</Text>
       </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => alert('Pressed')}
-      >
-        <Text>Cadatro Turma</Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()} >
+        <Text>voltar</Text>
       </TouchableOpacity>
-    </>
-    
+    </View>
+  );
+}
 
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}>
+          
+        </Stack.Screen>
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   button: {
-    alignItems: "center",
-    backgroundColor: "#CC9105",
-    padding: 10,
-    borderRadius: 6,
-    marginTop: 30,
-    marginHorizontal:20
+  alignItems: "center",
+  backgroundColor: "#CC9105",
+  padding: 10,
+  borderRadius: 6,
+  margin: 30,
+  marginHorizontal:20
   }
 });
+
+export default App;
